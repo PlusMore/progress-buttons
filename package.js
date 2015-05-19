1ac071e3f050e370e5b72cace971034802ceea9f
bfturner <brendanfturner@gmail.com>
2015-05-19 14:04:33 -0500

    made colors more universal

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'plusmore:progress-buttons',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Progress buttons inspired by codrops',
   // URL to the Git repository containing the source code for this package.
