4f5251c7ca05de4e91d42eec572c52e6af7ba3b9
Patrick Scott <patrick.lee.scott@gmail.com>
2015-05-18 23:07:35 -0400

    progress buttons

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'plusmore:progress-buttons',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('jquery', 'client');
+  api.use('less', 'client');
+  api.addFiles('progress-buttons.js', 'client');
+  api.addFiles('progress-buttons.less', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('plusmore:progress-buttons');
+  api.addFiles('progress-buttons-tests.js');
+});
